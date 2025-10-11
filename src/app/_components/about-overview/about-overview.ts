75cc2e122af4000259d6415069ff173c60d0647f
MrClaro <adryan.contatoprofissional@gmail.com>
2025-10-11 04:04:28 -0300

    feat: adiciona componente about-overview

---
+++ src/app/_components/about-overview/about-overview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-overview',
+  imports: [],
+  templateUrl: './about-overview.html',
+  styleUrl: './about-overview.scss'
+})
+export class AboutOverview {
+
+}
