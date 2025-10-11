9c8d478b528210be29ad160874223acfe7915f3a
MrClaro <adryan.contatoprofissional@gmail.com>
2025-10-11 04:04:38 -0300

    feat: adiciona componente skills-marquee

---
+++ src/app/_components/skills-marquee/skills-marquee.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-marquee',
+  imports: [],
+  templateUrl: './skills-marquee.html',
+  styleUrl: './skills-marquee.scss'
+})
+export class SkillsMarquee {
+
+}
