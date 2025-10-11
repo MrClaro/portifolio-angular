10a4a1c97d6f2a37aeb8a69503fc020b3e6726dc
MrClaro <adryan.contatoprofissional@gmail.com>
2025-10-11 04:04:56 -0300

    feat: adiciona página home

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { Component, HostListener } from '@angular/core';
+import { HeroSection } from '../../_components/hero-section/hero-section';
+
+@Component({
+  selector: 'app-home',
+  imports: [HeroSection],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+  scrollY = 0;
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    this.scrollY = window.scrollY;
+  }
+
+  scrollToContent() {
+    window.scrollTo({
+      top: window.innerHeight,
+      behavior: 'smooth',
+    });
+  }
+}
