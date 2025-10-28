5e84bd65eb7ac1776e5f7274425bf79823714447
MrClaro <adryan.contatoprofissional@gmail.com>
2025-10-28 02:58:43 -0300

    feat: adiciona elementos de teste

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,13 @@ import { Navbar } from './components/navbar/navbar';
 import { Home } from './pages/home/home';
 import { AboutOverview } from './components/about-overview/about-overview';
 import { ProjectsList } from './components/projects-list/projects-list';
+import { Expertise } from './components/expertise/expertise';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Home, AboutOverview, ProjectsList],
+  imports: [RouterOutlet, Navbar, Home, AboutOverview, ProjectsList, Expertise, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
