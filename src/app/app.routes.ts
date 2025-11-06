66a59cf6a4c8478378c064239da6fea4b350d055
MrClaro <adryan.contatoprofissional@gmail.com>
2025-11-06 16:23:41 -0300

    feat(routes): configura as rotas iniciais da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Layout } from './components/core/layout/layout';
+import { Home } from './pages/home/home';
+import { Projects } from './pages/projects/projects';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        component: Home,
+      },
+      {
+        path: 'projetos',
+        component: Projects,
+      },
+      {
+        path: 'sobre-mim',
+        component: Home,
+      },
+      {
+        path: 'blog',
+        component: Home,
+      },
+    ],
+  },
+];
