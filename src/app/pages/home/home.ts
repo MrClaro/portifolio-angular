f289bc5f2f6acbd3ee754c7ab1a13c70e1fd0382
MrClaro <adryan.contatoprofissional@gmail.com>
2025-11-06 16:24:27 -0300

    feat(page): finaliza página Home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,13 @@
 import { Component, HostListener } from '@angular/core';
-import { HeroSection } from '../../components/hero-section/hero-section';
+import { HeroSection } from '../../components/features/hero-section/hero-section';
+import { AboutOverview } from '../../components/features/about-overview/about-overview';
+import { Expertise } from '../../components/features/expertise/expertise';
+import { Contact } from '../../components/features/contact/contact';
+import { ProjectsList } from '../../components/features/projects-list/projects-list';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection],
+  imports: [HeroSection, AboutOverview, Expertise, ProjectsList, Contact],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
