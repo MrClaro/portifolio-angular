15e521f7cee57d7ea6f4c44d38ba36a3e67be1d0
MrClaro <adryan.contatoprofissional@gmail.com>
2025-11-06 16:23:15 -0300

    remove(app): remove elementos não utilizados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './components/navbar/navbar';
-import { Home } from './pages/home/home';
-import { AboutOverview } from './components/about-overview/about-overview';
-import { ProjectsList } from './components/projects-list/projects-list';
-import { Expertise } from './components/expertise/expertise';
-import { Contact } from './components/contact/contact';
-import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Home, AboutOverview, ProjectsList, Expertise, Contact, Footer],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
